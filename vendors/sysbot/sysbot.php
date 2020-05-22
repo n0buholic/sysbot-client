71f97b042feeabb63e8ae24adb54fdd63e9cda29
n0buholic <50941281+n0buholic@users.noreply.github.com>
2020-05-22 20:05:33 +0700

    added bypass SSL error

--- vendors/sysbot/sysbot.php
+++ vendors/sysbot/sysbot.php
@@ -37,6 +37,8 @@ class Sysbot {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
         $response = curl_exec($ch);
         return $response;
     }
